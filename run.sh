d61231098681cb42c387d2e905179b516ca27b51
Zubair Lutfullah Kakakhel <zubair@balena.io>
2018-12-27 12:45:32 +0000

    run.sh: Fix module directory path
    The module folder path was wrong. The module is built in a folder of
    the format example_module_device-type_os-version
    "example_module_intel-nuc_2.29.0+rev1.prod"
    
    RESIN_HOST_OS_VERSION is "balenaOS 2.26.0+rev3"
    
    Fix parsing to remove balenaOS and the space.
    
    The modules are built with .dev/.prod folder suffix. This information
    is not available inside the container. Use a delimiter * to make the
    module load. This loads dev/prod both if both are built and in the
    folder which is not ideal but better than the broken path we have
    currently
    
    Changelog-entry: Fix module directory path in run.sh
    Signed-off-by: Zubair Lutfullah Kakakhel <zubair@balena.io>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mod_dir="example_module_${RESIN_DEVICE_TYPE}_${RESIN_HOST_OS_VERSION}"
+OS_VERSION=$(echo $RESIN_HOST_OS_VERSION | cut -d " " -f 2)
+mod_dir="example_module_${RESIN_DEVICE_TYPE}_${OS_VERSION}*"
 
 insmod $mod_dir/hello.ko
 lsmod | grep hello
