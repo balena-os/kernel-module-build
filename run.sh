077dbb793f3a782f8a738685a0e50f6eb07b67b6
Kyle Harding <kyle@balena.io>
2020-11-10 10:32:09 -0500

    run.sh: build modules at runtime
    Move the module build steps to a runtime script.
    
    Although this often takes much longer since it is running on the
    target hardware, it can be worthwhile to have the device type
    and os version pulled from the running image.
    
    Change-type: major
    Changelog-entry: move the module build steps to runtime
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
-echo "OS Version is $OS_VERSION"
+trap 'balena-idle' ERR EXIT
 
-cd output
+OS_VERSION=$(echo "${BALENA_HOST_OS_VERSION}" | cut -d " " -f 2)
+
+echo "Device Type is ${BALENA_DEVICE_TYPE}"
+echo "OS Version is ${OS_VERSION}"
+
+./build.sh build --device "${BALENA_DEVICE_TYPE}" --os-version "${OS_VERSION}" --src example_module
+
+cd output || exit 1
 mod_dir="example_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
-for each in $mod_dir; do
-	echo Loading module from "$each"
-	insmod "$each/hello.ko"
+for each in ${mod_dir}
+do
+	echo Loading module from "${each}"
+	insmod "${each}/hello.ko"
 	lsmod | grep hello
 	rmmod hello
-done
-
-while true; do
-	sleep 60
 done
