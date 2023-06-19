012f4773edbf0a489094edcbee520cd16e3c5949
Alex Gonzalez <alexg@balena.io>
2023-06-19 16:37:35 +0200

    Refactor to use only kernel-modules-headers and make multi-container
    * Add LICENSE
    * Use multi-container docker compose
    * Simplify build script:
      * Pull headers directly from path instead of using S3 bucket parsing
      * Use only `kernel-modules-headers` instead of multiple redundant header files.
      * Always run `make modules_prepare`
      * Remove list and build functionality - just build the modules
      * Auto-detect the correct path stripping
      * Separate logging functions to a different script
    * Separate module load service from application
    
    TODO: convert into a block
    Change-type: major
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

---
+++ module/build.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+. include/logging
+
+set -o errexit
+set -o pipefail
+
+readonly script_name=$(basename "${0}")
+
+usage() {
+	cat <<EOF
+Usage: ${script_name} [OPTIONS]
+		-i Source directory (default to ./src)
+		-o Output directory (default to ./out)
+		-v balenaOS version (mandatory)
+		-s BalenaCloud slug name (mandatory)
+		-h Display usage
+EOF
+}
+
+fetch_headers()
+{
+	local slug="${1}"
+	local version="${2}"
+	local files_url="https://files.balena-cloud.com"
+	local esr_pattern="^[1-3][0-9]{3}\.(1|01|4|04|7|07||10)\.[0-9]*\.?(dev|prod)?$"
+	local image_path="images"
+	local filename
+	local url
+
+	if [[ ${version} =~ ${esr_pattern} ]]; then
+		image_path="esr-images"
+	fi
+
+	url="${files_url}/${image_path}/${slug}/${version//+/%2B}/kernel_modules_headers.tar.gz"
+	tmp_path=$(mktemp --directory)
+	cd $tmp_path
+
+	if ! wget --quiet $(echo "$url" | sed -e 's/+/%2B/g'); then
+		fail "Could not find headers for '$slug' at version '$version'"
+	fi
+
+	filename=$(basename $url)
+	# Count paths to strip by looking for .config and counting forward slashes
+	strip_depth=$(tar tf ${filename} | grep "/\.config$" | tr -dc / | wc -c)
+	if ! tar -xf $filename --strip $strip_depth; then
+		rm -rf "$tmp_path"
+		fail "Unable to extract $tmp_path/$filename."
+	fi
+	/usr/src/app/workarounds.sh "${slug}" "${version}" "${tmp_path}"
+	echo "${tmp_path}"
+}
+
+build_module() {
+	local headers_dir="${1}"
+	local output_dir="${2}"
+
+	mkdir -p "${output_dir}"
+	cd "${output_dir}"
+	make -C "${headers_dir}" modules_prepare
+	make -C "${headers_dir}" M="$PWD" modules
+	rm -rf "$headers_dir"
+}
+
+main() {
+	local src_dir=
+	local output_dir=
+	local os_version="${OS_VERSION}"
+	local slug=
+
+	## Sanity checks
+	if [ ${#} -eq 0 ] ; then
+		usage
+		exit 1
+	else
+		while getopts "hi:o:v:s:" c; do
+			case "${c}" in
+				i) src_dir="${OPTARG:-}";;
+				o) output_dir="${OPTARG:-}";;
+				v) os_version="${OPTARG:-}";;
+				s) slug="${OPTARG:-}";;
+				h) usage;;
+				*) usage;exit 1;;
+			esac
+		done
+
+		# Sanity checks
+		[ -z "${src_dir}" ] && fail "No module source directory provided"
+		[ -z "${output_dir}" ] && fail "No output directory provided"
+		[ -z "${os_version}" ] && fail "No OS versions specified"
+		[ -z "${slug}" ] && fail "No slugs specified"
+
+		output_dir="${output_dir}/${src_dir}_${slug}_${os_version}"
+		info "Building source from ${src_dir} into ${output_dir} for:
+			OS versions: ${os_version}
+			Device types: ${slug}"
+
+		rm -rf "$output_dir"
+		mkdir -p "$output_dir"
+		cp -dR "$src_dir"/* "$output_dir"
+
+		build_module $(fetch_headers "${slug}" "${os_version}") "${output_dir}"
+	fi
+}
+
+main "${@}"
